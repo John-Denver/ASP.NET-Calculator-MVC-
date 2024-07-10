502bfc1bde51a5efd3882c24329c57d9dd2b478b
john-denver <denverbogonko78@gmail.com>
2024-07-10 19:40:33 +0300

    group work

---
+++ Tip Calculator/Tip Calculator/Controllers/HomeController1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class TipCalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new TipCalculatorModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipCalculatorModel model)
+        {
+            if (model.MealCost <= 0)
+            {
+                model.ErrorMessage = "Please enter a valid meal cost greater than $0.00.";
+                return View(model);
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Clear()
+        {
+            var model = new TipCalculatorModel();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Tip Calculator/Tip Calculator/Models/TipCalculatorModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TipCalculator.Models
+{
+    public class TipCalculatorModel
+    {
+        public decimal MealCost { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public decimal CalculateTip(decimal percentage)
+        {
+            return MealCost * percentage / 100;
+        }
+    }
+}
